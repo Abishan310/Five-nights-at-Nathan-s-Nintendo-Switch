27c34d126d71e1d3e7508c77e1087610bf157c84
Abishan Ramesh <r.abishan31@gmail.com>
2026-03-15 19:50:01 +0000

    Add files via upload

---
+++ js/alps_config.js
@@ -0,0 +1,21 @@
+window.alpsClientInfo = {
+	clientId: 'f9a80c41a802ed10',
+	scope: [
+		'openid',
+		'user',
+		'user.mii',
+		'user.birthday',
+		'user.links[].id',
+		'user.wishlist',
+		'user.email',
+		'user.membership',
+		'missionStatus',
+		'missionStatus:progress',
+		'eshopPrice',
+		'eshopDemo',
+		'eshopDevice',
+		'pointWallet',
+		'userNotificationMessage:anyClients',
+		'userNotificationMessage:anyClients:write'
+	]
+}
